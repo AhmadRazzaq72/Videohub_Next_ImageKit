5ea5eef06d9718e0fdc9131fe29434869687c3d8
AhmadRazzaq72 <ahmadramay72@gmail.com>
2025-07-23 22:45:36 +0500

    reacted api's

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+
+export async function GET(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and password are required" },
+        { status: 400 }
+      );
+    }
+    await connectToDatabase();
+    const existinguser = await User.findOne({ email });
+    if (!existinguser) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    await User.create({
+      email,
+      password,
+    });
+
+    return NextResponse.json(
+      { message: "User created successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to create user" },
+      { status: 500 }
+    );
+  }
+}
